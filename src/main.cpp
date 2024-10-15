080b65545abbc9d53a277a91974b805b2774a2b4
AbdulazizAli3 <abdulaziz.ali@softwarfare.com>
2024-10-15 14:01:00 -0500

    Added main.cpp which is what askes the user for an equation which it then passes on to parse.cpp to be parsed which passes it on to calculatoins to be calculated.

---
+++ src/main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <cmath>
+#include <stdexcept>
+#include <string>
+
+#include "../include/ascii.hpp"
+#include "../include/parse.hpp"
+#include "../include/calculations.hpp"
+
+int main()
+{
+    // print a calulator and welcome message
+    std::cout << ascii::logo();
+    std::cout << "Welcom to Cocalc your go to quick calclulator!\n\n";
+
+    // get equation form user
+    std::string equation;
+    std::cout << "Enter a Mathimatical Equation (! to quit): ";
+    std::cin >> equation;
+
+    while (equation != "!")
+    {
+        // return equation solution
+        try
+        {
+            // solve the equation
+            parse::do_calculations(equation);
+
+            // get user input
+            std::cout << "Enter a Mathimatical Equation (! to quit): ";
+            std::cin >> equation;
+        }
+        catch(const std::runtime_error& e)
+        {
+            //catch divide by zero
+            std::cerr << e.what() << '\n';
+        }
+    }
+
+    return 0;
+}
